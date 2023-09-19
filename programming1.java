8d3d527edf1532572ec503b6da860f64f0846830
larissa-smith <94326675+larissa-smith@users.noreply.github.com>
2023-09-19 18:23:55 -0400

    Add files via upload

---
+++ programming1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author casey
+ */
+public class programming1 {
+    
+}
+public class Exercise_01_05 {
+	public static void main(String[] args) {
+		System.out.println("9.5 X 4.5 - 2.5 X 3");
+		System.out.println("-------------------");
+		System.out.println("     45.5 - 3.5");
+		System.out.println("         =");
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+	}
+}
+
---
+++ programming2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author casey
+ */
+public class programming2 {
+    
+}
+/*
+(Convert feet into meters) Write a program that reads a number in feet, converts it
+to meters, and displays the result. One foot is 0.305 meter. Here is a sample run:
+*/
+import java.util.Scanner;
+
+public class Exercise_02_03 {
+	public static void main(String[] args) {
+		// Create a Scanner object
+		Scanner input = new Scanner(System.in);
+
+		// Create constant value
+		final double METERS_PER_FOOT = 0.305;
+
+		// Prompt user to enter a number in feet
+		System.out.print("Enter a value for feet: ");
+		double feet = input.nextDouble();
+
+		// Convert feet into meters
+		double meters = feet * METERS_PER_FOOT;
+
+		// Display results
+		System.out.println(feet + " feet is " + meters + " meters");
+	}
+}
